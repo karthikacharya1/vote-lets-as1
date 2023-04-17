c5078861413c67ca014fae8cca9330a797c8f570
Karthik Acharya <129307817+karthikacharya1@users.noreply.github.com>
2023-04-17 07:24:19 +0530

    Add files via upload

---
+++ vote.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class vote{
+    public static void main(String args[])
+    {
+            int age=18;
+            if(age<=17)
+            System.out.println("You are age not eligible to vote");
+            else
+            System.out.println("you are eligible to vote");
+    }
+}
